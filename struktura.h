8fe60a6edb08108d1a665af9442069972bc7be1e
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-11 17:11:13 +0200

    Add files via upload
    Atskiras failas su struktūra studentas

---
+++ struktura.h
@@ -0,0 +1,15 @@
+#ifndef STRUKTURA_H
+#define STRUKTURA_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+std::string vardas="A";
+std::string pavarde="B";
+std::vector<int> paz;
+int egzaminas;
+double rezultatas=0;
+double mediana=0;
+};
+#endif
