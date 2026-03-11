d7b5cecd4208ae5fc63b224ab56e3d691289c906
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-11 23:02:58 +0200

    Add files via upload
    .h failas su funkcijomis

---
+++ Funkcijos.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "struktura.h"
+#include <vector>
+
+void RankinisIvedimas(std::vector<Studentas>& grupe);
+void RandomPazymiai(vector<Studentas>& grupe);
+void RandomStudentai(vector<Studentas>& grupe);
+void Nuskaitymas(vector<Studentas>& grupe, int &rusiavimas);
+void Isvedimas(vector<Studentas>& grupe, int pasirinkimas, int rusiavimas);
