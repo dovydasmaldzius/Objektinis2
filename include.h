03309f8ffa1262a47917430a4b0a9ab92cdcaf15
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-11 17:15:43 +0200

    Add files via upload
    Pakeičiau kodą, visas #include funkcijas pridėjau į atskirą failą include.h

---
+++ include.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <limits>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <chrono>
+#include <sstream>
+
+#endif
