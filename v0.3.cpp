cd21fadf039c75fde68d3bf44644eecb95fb591a
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-13 10:27:12 +0200

    Add files via upload
    Pradžioje esančius include<...> ir funkcijų prototipus perkėliau į atskirus failus

--- v0.3.cpp
+++ v0.3.cpp
@@ -1,18 +1,6 @@
 #include "struktura.h"
 #include "include.h"
-#include "using.h"
 #include "Funkcijos.h"
-#include "RankinisIvedimas.cpp"
-#include "RandomPazymiai.cpp"
-#include "RandomStudentai.cpp"
-#include "Nuskaitymas.cpp"
-#include "Isvedimas.cpp"
-
-void RankinisIvedimas(vector<Studentas>& grupe);
-void RandomPazymiai(vector<Studentas>& grupe);
-void RandomStudentai(vector<Studentas>& grupe);
-void Nuskaitymas(vector<Studentas>& grupe, int &rusiavimas);
-void Isvedimas(vector<Studentas>& grupe, int pasirinkimas, int rusiavimas);
 
     int main() {
 std::ios::sync_with_stdio(false); //duomenų srauto sinchronizavimas su C funkcijomis išjungiamas, kad būtų pasiektas didesnis efektyvumas
@@ -29,27 +17,27 @@ while(true) {
     cout<<"2 - studentu vardus ir pavardes ivesti ranka, pazymius generuoti atsitiktinai"<<endl;
     cout<<"3 - studentu vardus, pavardes ir pazymius generuoti atsitiktinai"<<endl;
     cout<<"4 - duomenis nuskaityti is failo"<<endl;
-try {
+try { //programos vieta, kurioje gali įvykti klaida
 cin>>pasirinkimas;
 if(cin.fail()) {
-    throw std::runtime_error("Prasome ivesti tik skaicius nuo 1 iki 4!");
+    throw std::runtime_error("Prasome ivesti tik skaicius nuo 1 iki 4!"); //tikrina, ar įvestas skaičius, jei ne - išmetamas klaidos pranešimas
 }
 if(pasirinkimas < 1 || pasirinkimas > 4) {
-    throw std::out_of_range("Prasome ivesti tik skaicius nuo 1 iki 4!");
+    throw std::out_of_range("Prasome ivesti tik skaicius nuo 1 iki 4!"); //tikrina, ar įvestas skaičius yra nuo 1 iki 4, jei ne - išmetamas klaidos pranešimas
 }
 break;
 }
-catch(const std::exception& e) {
-    cout << e.what() << endl;
-cin.clear();
-cin.ignore(numeric_limits<streamsize>::max(), '\n');
+catch(const std::exception& e) { //gaudoma bet kokia išimtis, kuri gali įvykti įvedant duomenis
+    cout << e.what() << endl; //išvedamas klaidos pranešimas
+cin.clear(); //klaida išvaloma, kad būtų galima vėl įvesti duomenis
+cin.ignore(numeric_limits<streamsize>::max(), '\n'); //pašalinami netinkami įvesties duomenys, kad būtų galima vėl įvesti duomenis
 }
 }
-
     if(pasirinkimas==1) RankinisIvedimas(grupe);
     else if(pasirinkimas==2) RandomPazymiai(grupe);
     else if(pasirinkimas==3) RandomStudentai(grupe);
     else if(pasirinkimas==4) Nuskaitymas(grupe, rusiavimas);
 Isvedimas(grupe, pasirinkimas, rusiavimas);
+
 return 0;
 };
