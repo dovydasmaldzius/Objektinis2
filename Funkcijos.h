20fb1bbbde48399d79720f181a346a08668feb2a
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-13 10:25:27 +0200

    Add files via upload
    Šiek tiek pakeičiau prototipą

--- Funkcijos.h
+++ Funkcijos.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef FUNKCIJOS_H //apsauga nuo kelių įtraukimų
+#define FUNKCIJOS_H //apsauga nuo kelių įtraukimų
 
 #include "struktura.h"
-#include <vector>
 
 void RankinisIvedimas(std::vector<Studentas>& grupe);
 void RandomPazymiai(vector<Studentas>& grupe);
 void RandomStudentai(vector<Studentas>& grupe);
 void Nuskaitymas(vector<Studentas>& grupe, int &rusiavimas);
 void Isvedimas(vector<Studentas>& grupe, int pasirinkimas, int rusiavimas);
+
+#endif //apsauga nuo kelių įtraukimų
