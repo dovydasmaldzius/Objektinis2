754f824e65315a7791d0c5e128c881c1c047de5d
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-26 21:34:48 +0200

    Add files via upload
    niekuo nepakeisti v0.3 užduoties failai

---
+++ funkcions.h
@@ -0,0 +1,12 @@
+#ifndef FUNKCIONS_H //apsauga nuo kelių įtraukimų
+#define FUNKCIONS_H //apsauga nuo kelių įtraukimų
+
+#include "structure.h"
+
+void RankinisIvedimas(std::vector<Studentas>& grupe);
+void RandomPazymiai(vector<Studentas>& grupe);
+void RandomStudentai(vector<Studentas>& grupe);
+void Nuskaitymas(vector<Studentas>& grupe, int &rusiavimas);
+void Isvedimas(vector<Studentas>& grupe, int pasirinkimas, int rusiavimas);
+
+#endif //apsauga nuo kelių įtraukimų
---
+++ includes.h
@@ -0,0 +1,34 @@
+#ifndef INCLUDES_H //apsauga nuo kelių įtraukimų
+#define INCLUDES_H //apsauga nuo kelių įtraukimų
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <limits>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <chrono>
+#include <sstream>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::left;
+using std::right;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::numeric_limits;
+using std::streamsize;
+using std::max;
+using std::ifstream;
+using std::getline;
+using std::ofstream;
+
+#endif //apsauga nuo kelių įtraukimų
