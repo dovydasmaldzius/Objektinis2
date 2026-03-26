7a2bb3581f1a78ca7fdd63b16a2f3fb73cbde635
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-26 21:33:26 +0200

    Add files via upload
    niekuo nepakeistas v0.3 užduoties failas

---
+++ StudentaiRandom.cpp
@@ -0,0 +1,58 @@
+#include "structure.h"
+#include "funkcions.h"
+#include "includes.h"
+
+void RandomStudentai(vector<Studentas>& grupe) {
+
+Studentas A;
+
+vector<string> firstNames={"Jonas","Petras","Ona","Lina","Mantas","Egle","Darius","Ruta","Tomas","Ieva"};
+vector<string> lastNames={"Petraitis","Kazlauskas","Jankauskas","Stankevicius","Vilkas","Daugela","Miskinis","Jankauskiene","Zalys","Baranauskas"};
+
+char testi='t';
+int ii=1;
+
+while(testi=='t' || testi=='T') {
+A.vardas = firstNames[rand() % firstNames.size()];
+A.pavarde = lastNames[rand() % lastNames.size()];
+
+int n = rand()%10 + 1;
+int sum = 0;
+
+cout<<"Sugeneruotas "<<ii<<" studentas: "<<A.vardas<<" "<<A.pavarde<<"\n";
+cout<<"Random namu darbu pazymiai:\n";
+
+for(int i=0;i<n;i++) {
+int temp = rand()%10 + 1;
+
+A.paz.push_back(temp);
+sum += temp;
+    cout<<temp<<" ";
+}
+cout<<endl;
+
+A.egzaminas = (rand() % 10) + 1;
+cout<<"Random egzamino pazymys: "<<A.egzaminas<<endl;
+
+A.paz.push_back(A.egzaminas);
+
+sort(A.paz.begin(), A.paz.end());
+
+int kiekis = A.paz.size();
+
+if(kiekis == 0) A.mediana = 0;
+else if(kiekis % 2 == 0) A.mediana = (A.paz[kiekis/2 - 1] + A.paz[kiekis/2]) / 2.0;
+else A.mediana = A.paz[kiekis/2];
+
+A.paz.pop_back();
+
+A.rezultatas = sum*1.0/(n*1.0)*0.4 + A.egzaminas*0.6;
+grupe.push_back(A);
+A.paz.clear(); 
+cout<<"Spauskite t, jei norite sugeneruoti kita studenta "<<endl;
+cout<<"Jei baigete studentu generavima, spauskite bet koki (ne t) simboli"<<endl;
+cin>>testi;
+
+ii++;
+}
+}
