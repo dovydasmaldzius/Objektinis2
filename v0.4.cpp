a441ed2f1049191ae8b24247e72dea64cfe7b4f7
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-03-27 09:36:45 +0200

    Add files via upload
    Pakeičiau failą, kad būtų galima generuoti atsitiktinius failus (pradžiai tik trys, iki 100000 studentų)

--- v0.4.cpp
+++ v0.4.cpp
@@ -16,13 +16,14 @@ while(true) {
     cout<<"2 - studentu vardus ir pavardes ivesti ranka, pazymius generuoti atsitiktinai"<<endl;
     cout<<"3 - studentu vardus, pavardes ir pazymius generuoti atsitiktinai"<<endl;
     cout<<"4 - duomenis nuskaityti is failo"<<endl;
+    cout<<"5 - generuoti failus"<<endl;
 try { //programos vieta, kurioje gali įvykti klaida
 cin>>pasirinkimas;
 if(cin.fail()) {
-    throw std::runtime_error("Prasome ivesti tik skaicius nuo 1 iki 4!"); //tikrina, ar įvestas skaičius, jei ne - išmetamas klaidos pranešimas
+    throw std::runtime_error("Prasome ivesti tik skaicius nuo 1 iki 5!"); //tikrina, ar įvestas skaičius, jei ne - išmetamas klaidos pranešimas
 }
-if(pasirinkimas < 1 || pasirinkimas > 4) {
-    throw std::out_of_range("Prasome ivesti tik skaicius nuo 1 iki 4!"); //tikrina, ar įvestas skaičius yra nuo 1 iki 4, jei ne - išmetamas klaidos pranešimas
+if(pasirinkimas < 1 || pasirinkimas > 5) {
+    throw std::out_of_range("Prasome ivesti tik skaicius nuo 1 iki 5!"); //tikrina, ar įvestas skaičius yra nuo 1 iki 5, jei ne - išmetamas klaidos pranešimas
 }
 break;
 }
@@ -36,7 +37,21 @@ cin.ignore(numeric_limits<streamsize>::max(), '\n'); //pašalinami netinkami įv
     else if(pasirinkimas==2) RandomPazymiai(grupe);
     else if(pasirinkimas==3) RandomStudentai(grupe);
     else if(pasirinkimas==4) Nuskaitymas(grupe, rusiavimas);
-Isvedimas(grupe, pasirinkimas, rusiavimas);
+    else if(pasirinkimas==5) {
 
+vector<int> dydziai={1000,10000,100000};
+
+for(int n : dydziai){
+string failas="studentai" + to_string(n)+".txt";
+
+auto start = std::chrono::high_resolution_clock::now();
+GeneruotiFaila(failas,n);
+
+auto end = std::chrono::high_resolution_clock::now();
+std::chrono::duration<double> diff = end - start;
+
+cout<<failas<<" sukurtas per "<<diff.count()<<" s\n"; }
+}
+Isvedimas(grupe, pasirinkimas, rusiavimas);
 return 0;
 };
