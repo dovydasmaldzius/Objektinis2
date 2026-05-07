996d8a270396d039f455faf70310f543f019da3e
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-07 23:34:47 +0300

    Add files via upload
    Pakeičiau laiko testavimo funkciją, kad spausdintų nuskaitymo, rūšiavimo ir skirstymo (3 strategijos) su vector/list/deque konteineriais

--- testasv1.0.cpp
+++ testasv1.0.cpp
@@ -1,46 +1,65 @@
 #include "structure.h"
 #include "funkcions.h"
 #include "includes.h"
+#include "Strategijos.h"
 
 template <typename Container>
 void TestuotiGreiti(const string& failas) {
-
 Container studentai;
 
-auto t1 = std::chrono::high_resolution_clock::now();
+auto ns = std::chrono::high_resolution_clock::now();
 NuskaitymasIsFailo(failas, studentai);
+auto ne = std::chrono::high_resolution_clock::now();
 
-auto t2 = std::chrono::high_resolution_clock::now();
+cout<<failas<<"\n";
+cout<<"Nuskaitymas: "<< duration<double>(ne-ns).count()<<" s\n";
 
-auto t3 = std::chrono::high_resolution_clock::now();
 
-if constexpr (std::is_same<Container, std::list<Studentas>>::value) {
-studentai.sort([](const Studentas& a, const Studentas& b) {
+Container rusiuoti = studentai;
+auto rs = std::chrono::high_resolution_clock::now();
+
+if constexpr (std::is_same_v<Container, std::list<Studentas>>) { //tikrina, ar konteineris yra list, jei taip - naudojamas list sort metodas
+    rusiuoti.sort([](const Studentas& a, const Studentas& b) {
 return a.rezultatas < b.rezultatas; });
-} 
+}
 else {
-std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, const Studentas& b) {
+std::sort(rusiuoti.begin(), rusiuoti.end(), [](const Studentas& a, const Studentas& b) {
 return a.rezultatas < b.rezultatas; });
 }
 
-auto t4 = std::chrono::high_resolution_clock::now();
+auto re = std::chrono::high_resolution_clock::now();
+cout<<"Rusiavimas: "<<duration<double>(re-rs).count()<<" s\n";
 
-Container vargsiukai;
-Container kietiakai;
 
-auto t5 = std::chrono::high_resolution_clock::now();
+{ //Strategija nr. 1
+Container kopija = rusiuoti;
+Container vargsiukai, kietiakai;
 
-for(const auto& s : studentai){
-if(s.rezultatas < 5)
-    vargsiukai.push_back(s);
-else
-kietiakai.push_back(s);
+auto s = std::chrono::high_resolution_clock::now();
+Strategija1(kopija, vargsiukai, kietiakai);
+auto e = std::chrono::high_resolution_clock::now();
+
+cout<<"Strategija 1: "<<duration<double>(e-s).count()<<" s\n";
 }
 
-auto t6 = std::chrono::high_resolution_clock::now();
+{ //Strategija nr. 2
+Container kopija = rusiuoti;
+Container vargsiukai;
 
-    cout<<failas<<endl;
-    cout<<"Nuskaitymas: "<<std::chrono::duration<double>(t2-t1).count()<<" s\n";
-    cout<<"Rusiavimas: "<<std::chrono::duration<double>(t4-t3).count()<<" s\n";
-    cout<<"Skirstymas: "<<std::chrono::duration<double>(t6-t5).count()<<" s\n\n";
+auto s = std::chrono::high_resolution_clock::now();
+Strategija2(kopija, vargsiukai);
+auto e = std::chrono::high_resolution_clock::now();
+
+cout<<"Strategija 2: "<<duration<double>(e-s).count()<<" s\n";
+}
+
+{ //Strategija nr. 3
+Container kopija = rusiuoti;
+Container vargsiukai, kietiakai;
+
+auto s = std::chrono::high_resolution_clock::now();
+Strategija3(kopija, vargsiukai, kietiakai);
+auto e = std::chrono::high_resolution_clock::now();
+
+cout<<"Strategija 3: "<<duration<double>(e-s).count()<<" s\n\n"; }
 }
