bd47ddf138c6d325d67a6f773d57975bc42c4622
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-08 09:44:17 +0300

    Add files via upload
    Pakeičiau šį failą, kad veiktų su visomis strategijomis pagal užduotį

--- Strategijos.h
+++ Strategijos.h
@@ -7,7 +7,7 @@
 
 template<typename Container>
 void Strategija1(Container& studentai, Container& vargsiukai, Container& kietiakai) { 
-    for(const auto& s : studentai){
+    for(const auto& s : studentai) {
 if(s.rezultatas < 5)
     vargsiukai.push_back(s);
 else
@@ -16,21 +16,22 @@ else
 
 template<typename Container>
 void Strategija2(Container& studentai, Container& vargsiukai) {
-std::remove_copy_if(studentai.begin(), studentai.end(), std::back_inserter(vargsiukai),
-[](const Studentas& s){ return s.rezultatas >= 5; });
+    std::remove_copy_if (studentai.begin(), studentai.end(), std::back_inserter(vargsiukai), [](const Studentas& s) {
+return s.rezultatas >= 5; });
 
-auto it = std::remove_if(studentai.begin(), studentai.end(), [](const Studentas& s){ return s.rezultatas < 5; });
+auto it = std::remove_if( studentai.begin(), studentai.end(), [](const Studentas& s) {
+return s.rezultatas < 5; });
+
 studentai.erase(it, studentai.end());
 }
 
 template<typename Container>
 void Strategija3(Container& studentai, Container& vargsiukai, Container& kietiakai) {
-auto it = std::partition(
-studentai.begin(), studentai.end(), [](const Studentas& s) {
+auto it = std::partition (studentai.begin(), studentai.end(), [](const Studentas& s) {
 return s.rezultatas < 5; });
 
-vargsiukai.insert(vargsiukai.end(), studentai.begin(), it);
-kietiakai.insert(kietiakai.end(), it, studentai.end());
+vargsiukai.insert(vargsiukai.end(), studentai.begin(), it); //vargsiukai konteineris užpildomas vargšiukais
+kietiakai.insert(kietiakai.end(), it, studentai.end()); //kietiakai konteineris užpildomas kietiakais
 }
 
 #endif //apsauga nuo kelių įtraukimų
