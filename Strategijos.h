a7e93bdd9d090ec733c8252a2bbec714f816d53b
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-16 13:42:32 +0300

    Add files via upload
    Pakeičiau šį buvusį kodą, kad programa veiktų su klasėmis vietoje struktūros

--- Strategijos.h
+++ Strategijos.h
@@ -1,26 +1,27 @@
 #ifndef STRATEGIJOS_H //apsauga nuo kelių įtraukimų
 #define STRATEGIJOS_H //apsauga nuo kelių įtraukimų
 
-#include "structure.h"
+#include "Studentas.h"
 #include "funkcions.h"
 #include "includes.h"
 
 template<typename Container>
 void Strategija1(Container& studentai, Container& vargsiukai, Container& kietiakai) { 
     for(const auto& s : studentai) {
-if(s.rezultatas < 5)
+if(s.rezultatas() < 5)
     vargsiukai.push_back(s);
 else
     kietiakai.push_back(s); }
 }
 
 template<typename Container>
-void Strategija2(Container& studentai, Container& vargsiukai) {
-    std::remove_copy_if (studentai.begin(), studentai.end(), std::back_inserter(vargsiukai), [](const Studentas& s) { 
-return s.rezultatas >= 5; }); //studentai konteineryje paliekami tik tie, kurių rezultatas didesnis arba lygus 5 (kietiakai)
-
-auto it = std::remove_if( studentai.begin(), studentai.end(), [](const Studentas& s) { // jei studentas yra vargšiukas, jis pašalinamas iš studentų konteinerio
-return s.rezultatas < 5; });
+void Strategija2(Container& studentai,Container& vargsiukai) {
+auto it = std::remove_if(studentai.begin(), studentai.end(), [&](const Studentas& s){
+    if(s.rezultatas() < 5) {
+vargsiukai.push_back(s);
+return true;
+}
+return false; });
 
 studentai.erase(it, studentai.end());
 }
@@ -28,7 +29,7 @@ studentai.erase(it, studentai.end());
 template<typename Container>
 void Strategija3(Container& studentai, Container& vargsiukai, Container& kietiakai) {
 auto it = std::partition (studentai.begin(), studentai.end(), [](const Studentas& s) { //padalina studentus į dvi grupes
-return s.rezultatas < 5; }); //grupė su vargšiukais bus prieš grupę su kietiakais
+return s.rezultatas() < 5; }); //grupė su vargšiukais bus prieš grupę su kietiakais
 
 vargsiukai.insert(vargsiukai.end(), studentai.begin(), it); //vargsiukai konteineris užpildomas vargšiukais
 kietiakai.insert(kietiakai.end(), it, studentai.end()); //kietiakai konteineris užpildomas kietiakais
