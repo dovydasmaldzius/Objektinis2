5f2b94c826ee8bfde5879ed2ee82f6f6b77d26ea
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-16 13:42:46 +0300

    Add files via upload
    Pakeičiau šį buvusį kodą, kad programa veiktų su klasėmis vietoje struktūros

--- PazymiaiRandom.cpp
+++ PazymiaiRandom.cpp
@@ -1,4 +1,4 @@
-#include "structure.h"
+#include "Studentas.h"
 #include "funkcions.h"
 #include "includes.h"
 
@@ -13,8 +13,8 @@ int sum=0;
 while(true) {
     cout<<"Iveskite varda ir pavarde: \n";
 try { //programos vieta, kurioje gali įvykti klaida
-cin>>A.vardas>>A.pavarde;
-bool hasDigit = std::any_of(A.vardas.begin(), A.vardas.end(), [](unsigned char c){ return std::isdigit(c); }) || std::any_of(A.pavarde.begin(), A.pavarde.end(), [](unsigned char c){ return std::isdigit(c); });
+cin>>A.vardas()>>A.pavarde();
+bool hasDigit = std::any_of(A.vardas().begin(), A.vardas().end(), [](unsigned char c){ return std::isdigit(c); }) || std::any_of(A.pavarde().begin(), A.pavarde().end(), [](unsigned char c){ return std::isdigit(c); });
 if(hasDigit) {
     throw std::runtime_error("Prasome ivesti tik raides!");
     continue; }
@@ -36,30 +36,30 @@ while(testipaz=='t' || testipaz=='T') {
 temp = (rand() % 10) + 1;
 cout<<"Random pazymys: "<<temp<<endl;
 
-A.paz.push_back(temp);
+A.paz().push_back(temp);
 sum+=temp;
     cout<<"Spauskite t, jei norite ivesti kita pazymi "<<endl;
     cout<<"Jei baigete pazymiu ivedima, spauskite bet koki (ne t) simboli"<<endl;
 cin>>testipaz;
 }
         
-A.egzaminas = (rand() % 10) + 1;
-cout<<"Sugeneruotas egzamino pazymys: "<<A.egzaminas<<endl;
+A.egzaminas() = (rand() % 10) + 1;
+cout<<"Sugeneruotas egzamino pazymys: "<<A.egzaminas()<<endl;
 
-A.paz.push_back(A.egzaminas);
+A.paz().push_back(A.egzaminas());
         
-sort(A.paz.begin(), A.paz.end());
+std::sort(A.paz().begin(), A.paz().end());
         
-int kiekis = A.paz.size();
+int kiekis = A.paz().size();
         
-if(kiekis == 0) A.mediana = 0;
-else if(kiekis % 2 == 0) A.mediana = (A.paz[kiekis/2 - 1] + A.paz[kiekis/2]) / 2.0;
-else A.mediana = A.paz[kiekis/2];
-A.paz.pop_back();
+if(kiekis == 0) A.mediana() = 0;
+else if(kiekis % 2 == 0) A.mediana() = (A.paz()[kiekis/2 - 1] + A.paz()[kiekis/2]) / 2.0;
+else A.mediana() = A.paz()[kiekis/2];
+A.paz().pop_back();
         
-A.rezultatas = (sum * 1.0 / A.paz.size()) * 0.4 + A.egzaminas * 0.6;
+A.rezultatas() = (sum * 1.0 / A.paz().size()) * 0.4 + A.egzaminas() * 0.6;
 grupe.push_back(A);
-A.paz.clear(); 
+A.paz().clear(); 
     cout<<"Spauskite t, jei norite ivesti kita studenta "<<endl;
     cout<<"Jei baigete studentu ivedima, spauskite bet koki (ne t) simboli"<<endl;
 cin>>testistud;
