7ee9938ebbe6ef08c75329793a30cbf1c2f688fc
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-16 13:42:20 +0300

    Add files via upload
    Pakeičiau šį buvusį kodą, kad programa veiktų su klasėmis vietoje struktūros

--- StudentaiRandom.cpp
+++ StudentaiRandom.cpp
@@ -1,4 +1,4 @@
-#include "structure.h"
+#include "Studentas.h"
 #include "funkcions.h"
 #include "includes.h"
 
@@ -14,42 +14,42 @@ char testi='t';
 int ii=1;
 
 while(testi=='t' || testi=='T') {
-A.vardas = firstNames[rand() % firstNames.size()];
-A.pavarde = lastNames[rand() % lastNames.size()];
+A.vardas() = firstNames[rand() % firstNames.size()];
+A.pavarde() = lastNames[rand() % lastNames.size()];
 
 int n = rand()%10 + 1;
 int sum = 0;
 
-cout<<"Sugeneruotas "<<ii<<" studentas: "<<A.vardas<<" "<<A.pavarde<<"\n";
+cout<<"Sugeneruotas "<<ii<<" studentas: "<<A.vardas()<<" "<<A.pavarde()<<"\n";
 cout<<"Random namu darbu pazymiai:\n";
 
 for(int i=0;i<n;i++) {
 int temp = rand()%10 + 1;
 
-A.paz.push_back(temp);
+A.paz().push_back(temp);
 sum += temp;
     cout<<temp<<" ";
 }
 cout<<endl;
 
-A.egzaminas = (rand() % 10) + 1;
-cout<<"Random egzamino pazymys: "<<A.egzaminas<<endl;
+A.egzaminas() = (rand() % 10) + 1;
+cout<<"Random egzamino pazymys: "<<A.egzaminas()<<endl;
 
-A.paz.push_back(A.egzaminas);
+A.paz().push_back(A.egzaminas());
 
-sort(A.paz.begin(), A.paz.end());
+std::sort(A.paz().begin(), A.paz().end());
 
-int kiekis = A.paz.size();
+int kiekis = A.paz().size();
 
-if(kiekis == 0) A.mediana = 0;
-else if(kiekis % 2 == 0) A.mediana = (A.paz[kiekis/2 - 1] + A.paz[kiekis/2]) / 2.0;
-else A.mediana = A.paz[kiekis/2];
+if(kiekis == 0) A.mediana() = 0;
+else if(kiekis % 2 == 0) A.mediana() = (A.paz()[kiekis/2 - 1] + A.paz()[kiekis/2]) / 2.0;
+else A.mediana() = A.paz()[kiekis/2];
 
-A.paz.pop_back();
+A.paz().pop_back();
 
-A.rezultatas = sum*1.0/(n*1.0)*0.4 + A.egzaminas*0.6;
+A.rezultatas() = sum*1.0/(n*1.0)*0.4 + A.egzaminas()*0.6;
 grupe.push_back(A);
-A.paz.clear(); 
+A.paz().clear(); 
 cout<<"Spauskite t, jei norite sugeneruoti kita studenta "<<endl;
 cout<<"Jei baigete studentu generavima, spauskite bet koki (ne t) simboli"<<endl;
 cin>>testi;
