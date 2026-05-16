6a880061499883c586cb718799350d68a4427af6
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-16 13:41:29 +0300

    Add files via upload
    Pakeičiau šį buvusį kodą, kad programa veiktų su klasėmis vietoje struktūros

--- VisuFailuNuskaitymas.cpp
+++ VisuFailuNuskaitymas.cpp
@@ -1,44 +1,54 @@
-#include "structure.h"
+#include "Studentas.h"
 #include "funkcions.h"
 #include "includes.h"
 
 template<typename Container>
 void NuskaitymasIsFailo(const std::string& failas, Container& grupe) {
-    
+
 ifstream fd(failas);
 if(!fd) {
-cout<<"FAILAS NERASTAS: "<<failas<<endl;
-return;
-}
+    cout<<"FAILAS NERASTAS: "<<failas<<endl;
+return; }
 
 string temp;
 getline(fd, temp);
-Studentas A;
 
 while(getline(fd, temp)) {
-
+Studentas A;
 std::stringstream ss(temp);
+string vardas,pavarde;
+ss>>vardas>>pavarde;
 
-ss>>A.vardas>>A.pavarde;
+A.setVardas(vardas);
+A.setPavarde(pavarde);
 vector<int> pazymiai;
 int pazymys;
-int sum=0;
-while(ss >> pazymys)
+int sum = 0;
+
+while(ss>>pazymys)
 pazymiai.push_back(pazymys);
 
-A.egzaminas = pazymiai.back();
+A.setEgzaminas(pazymiai.back());
 pazymiai.pop_back();
-A.paz = pazymiai;
 
-for(int p:A.paz)
-sum += p;
+A.setPaz(pazymiai);
+for(int p : A.paz())
+    sum += p;
 
-sort(A.paz.begin(), A.paz.end());
-int kiekis = A.paz.size();
+vector<int> tempPaz = A.paz();
+std::sort(tempPaz.begin(), tempPaz.end());
+int kiekis = tempPaz.size();
+double mediana;
     if(kiekis % 2 == 0)
-A.mediana = (A.paz[kiekis/2-1]+A.paz[kiekis/2])/2.0;
-    else A.mediana = A.paz[kiekis/2];
-A.rezultatas = (sum*1.0/A.paz.size())*0.4 + A.egzaminas*0.6;
+mediana = (tempPaz[kiekis/2-1] + tempPaz[kiekis/2]) / 2.0;
+    else
+mediana = tempPaz[kiekis/2];
+
+A.setMediana(mediana);
+
+double rez = (sum * 1.0 / A.paz().size()) * 0.4 +
+A.egzaminas() * 0.6;
+A.setRezultatas(rez);
 grupe.push_back(A);
 }
 fd.close();
