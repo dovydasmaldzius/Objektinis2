a6d7d7b4986ee2da9318519a320eb0c50d30671a
Dovydas Maldžius <dovmaldzius@gmail.com>
2026-05-29 00:19:16 +0300

    Add files via upload
    Pakeičiau kodą, kad galėčiau naudoti savo sukurtą konteinerį Vector vietoje std::vector

--- LaikoMatavimas.cpp
+++ LaikoMatavimas.cpp
@@ -4,19 +4,20 @@
 
 void LaikoMatavimas()
 {
-vector<string> failai = {
-    "studentai1000.txt",
-    "studentai10000.txt",
-    "studentai100000.txt",
-    "studentai1000000.txt",
-    "studentai10000000.txt"
+    std::vector<string> failai = {
+"studentai1000.txt",
+"studentai10000.txt",
+"studentai100000.txt",
+"studentai1000000.txt",
+"studentai10000000.txt"
 };
 
-for(const auto& f : failai) { //atliekami testai su visais sugeneruotais failais su vectoriaus, listo ir deko konteineriais
-    cout<<"VECTOR\n";
+for(const auto& f : failai) {
+    cout << "=== " << f << " ===\n";
+    cout << "SU VECTOR (Vector<Studentas>)\n";
 TestuotiGreiti<vector<Studentas>>(f);
-    cout<<"LIST\n";
-TestuotiGreiti<list<Studentas>>(f);
-    cout<<"DEQUE\n";
-TestuotiGreiti<deque<Studentas>>(f); }
+    cout << "SU STD VECTOR (std::vector<Studentas>)\n";
+TestuotiGreiti<std::vector<Studentas>>(f);
+    cout << "\n";
+}
 }
